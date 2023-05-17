d752c9d3232f86df6880f44a7d73ef8b92c9ab29
Cody Doucette <doucette@bu.edu>
2023-05-17 16:04:51 -0400

    Add initial files

---
+++ llset.py
@@ -0,0 +1,23 @@
+class LLSet:
+    class Node:
+        def __init__(self, val, next):
+            self.val = val
+            self.next = next
+
+    def __init__(self):
+        self.head = None
+
+    def insert(self, item):
+        pass
+
+    def contains(self, item):
+        pass
+
+    def delete(self, item):
+        pass
+
+    def size(self):
+        pass
+
+    def to_list(self):
+        pass
