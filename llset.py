6dacf7e3a11181d53f0a7c57dbc0c9d2943ad481
Cody Doucette <doucette@bu.edu>
2023-05-17 16:13:45 -0400

    Add my solution

--- llset.py
+++ llset.py
@@ -6,18 +6,50 @@ class LLSet:
 
     def __init__(self):
         self.head = None
+        self.num_items = 0
 
     def insert(self, item):
-        pass
+        trav = self.head
+        while trav is not None:
+            if item == trav.val:
+                return False
+            trav = trav.next
+        new_node = LLSet.Node(item, self.head)
+        self.head = new_node
+        self.num_items += 1
+        return True
 
     def contains(self, item):
-        pass
+        trav = self.head
+        while trav is not None:
+            if item == trav.val:
+                return True
+            trav = trav.next
+        return False
 
     def delete(self, item):
-        pass
+        prev = None
+        trav = self.head
+
+        while trav is not None:
+            if item == trav.val:
+                if prev is not None:
+                    prev.next = trav.next
+                else:
+                    self.head = trav.next
+                return True
+            prev = trav
+            trav = trav.next
+
+        return False
 
     def size(self):
-        pass
+        return self.num_items
 
     def to_list(self):
-        pass
+        lst = []
+        trav = self.head
+        while trav is not None:
+            lst.append(trav.val)
+            trav = trav.next
+        return lst
